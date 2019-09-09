cdc5a06be285521176936451c8be6b3833a14e31
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-09 16:21:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,12 +4,20 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleep(1)
+    # sleep(1)
   end
 
   p "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
+
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+
+  p "HAPPY NEW YEAR!"
 
 end
